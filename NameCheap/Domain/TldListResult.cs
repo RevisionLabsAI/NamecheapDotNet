52cbc79006e574357e9b49407f1861f71af84625
Bryan Kruman <bryankruman@gmail.com>
2025-07-05 12:24:29 -0400

    Add functionality around TldListResult(s)

--- NameCheap/Domain/TldListResult.cs
+++ NameCheap/Domain/TldListResult.cs
@@ -1,10 +1,12 @@
-﻿using System.Xml.Serialization;
+﻿using System;
+using System.Xml.Serialization;
 
 namespace NameCheap
 {
     [XmlRoot("Tlds")]
     public class TldListResult
     {
+        public DateTime TimeStamp { get; set; }
         [XmlElement("Tld")]
         public Tld[] Tlds { get; set; }
     }
